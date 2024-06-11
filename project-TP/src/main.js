7aaf6a4917a5191b9f89973a2f0747354cd510a4
minjeonj0 <whalswjd99@gmail.com>
2024-06-11 10:35:14 +0900

    main.js update

--- project-TP/src/main.js
+++ project-TP/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import router from './router/index.js'
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount('#app')
---
+++ project-TP/src/pages/CreateItem.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="bg-[#FFFFFF] flex flex-col items-center p-[0_0_205px_0] w-[1440px] box-sizing-border">
+  <div class="bg-[#F0ECCA] m-[0_0_134px_0] flex flex-row justify-between p-[11px_36px_7px_70px] w-[1440px] box-sizing-border">
+    <div class="bg-[url('../assets/images/potato_50399956401.png')] bg-[50%_50%] bg-cover bg-no-repeat w-[45.1px] h-[60px]">
+    </div>
+    <div class="m-[21.5px_0_17.5px_0] flex flex-row justify-between w-[171.6px] h-[fit-content] box-sizing-border">
+      <span class="m-[0_10.5px_0_0] w-[82.5px] break-words font-['Inter'] font-normal text-[16px] leading-[1.3] text-[#000000]">
+        Home
+      </span>
+      <span class="break-words font-['Inter'] font-normal text-[16px] leading-[1.3] text-[#000000]">
+        Total View
+      </span>
+    </div>
+    <div class="m-[21.5px_0_17.5px_0] inline-block break-words font-['Inter'] font-normal text-[16px] leading-[1.3] text-[#000000]">
+      Summary
+    </div>
+    <div class="m-[13px_0_13.8px_0] flex flex-row justify-between w-[162px] h-[fit-content] box-sizing-border">
+      <img class="m-[3px_0_0.2px_0] w-[30px] h-[30px]" src="../assets/vectors/vector_1_x2.svg" />
+      <div class="m-[0_0_3.2px_0] flex w-[30px] h-[30px] box-sizing-border">
+        <img class="w-[30px] h-[30px]" src="../assets/vectors/vector_17_x2.svg" />
+      </div>
+      <div class="m-[3.2px_0_0_0] flex w-[30px] h-[30px] box-sizing-border">
+        <img class="w-[30px] h-[30px]" src="../assets/vectors/vector_12_x2.svg" />
+      </div>
+    </div>
+  </div>
+  <div class="m-[0_137px_23px_129px] flex flex-row justify-between w-[1174px] box-sizing-border">
+    <div class="m-[0_26px_4px_0] inline-block w-[869px] break-words font-['Inter'] font-semibold text-[40px] leading-[1.3] text-[#000000]">
+      신규 등록
+    </div>
+    <div class="m-[8px_0_0_0] flex flex-row w-[279px] h-[fit-content] box-sizing-border">
+      <div class="shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[4px] bg-[#FAFAF5] m-[0_27px_0_0] flex p-[13.5px_0_13.5px_0] w-[126px] box-sizing-border">
+        <span class="break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#000000]">
+          취소
+        </span>
+      </div>
+      <div class="shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] rounded-[4px] bg-[#FAFAF5] flex p-[13.5px_0_13.5px_0] w-[126px] box-sizing-border">
+        <span class="break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#000000]">
+          등록
+        </span>
+      </div>
+    </div>
+  </div>
+  <div class="bg-[#F0ECCA] relative m-[0_129px_0_129px] flex flex-row p-[157px_0_134px_0] w-[1182px] box-sizing-border">
+    <div class="rounded-[20px] bg-[#FFFFFF] absolute top-[101px] right-[97px] w-[972px] h-[325px]">
+    </div>
+    <div class="relative m-[5px_27.5px_6px_0] flex flex-col box-sizing-border">
+      <div class="m-[0_0_40px_0] inline-block self-start break-words font-['Inter'] font-bold text-[16px] text-[#000000]">
+        날짜
+      </div>
+      <div class="m-[0_0_49px_0] inline-block self-start break-words font-['Inter'] font-bold text-[16px] text-[#000000]">
+        금액
+      </div>
+      <div class="m-[0_0_61px_0] inline-block break-words font-['Inter'] font-bold text-[16px] text-[#000000]">
+        수입 / 지출
+      </div>
+      <span class="m-[0_13.7px_0_0] break-words font-['Inter'] font-bold text-[16px] text-[#000000]">
+        카테고리
+      </span>
+    </div>
+    <div class="relative m-[0_57px_0_0] flex flex-col box-sizing-border">
+      <div class="rounded-[20px] border-[2px_solid_rgba(0,0,0,0.06)] bg-[#FFFFFF] m-[0_0_29px_0] flex flex-row justify-between p-[0_15px_0_14px] w-[306px] box-sizing-border">
+        <div class="m-[2.5px_10.5px_2.5px_0] inline-block w-[237.5px] break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#6D6D6D]">
+          2024. 06. 10
+        </div>
+        <div class="flex w-[25px] h-[28px] box-sizing-border">
+          <img class="w-[25px] h-[28px]" src="../assets/vectors/vector_20_x2.svg" />
+        </div>
+      </div>
+      <div class="rounded-[20px] border-[2px_solid_rgba(0,0,0,0.06)] bg-[#FFFFFF] m-[0_0_37px_0] p-[2.5px_14px_2.5px_14px] w-[fit-content] box-sizing-border">
+        <span class="break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#6D6D6D]">
+          10,000
+        </span>
+      </div>
+      <div class="rounded-[20px] border-[2px_solid_rgba(0,0,0,0.06)] bg-[#FFFFFF] m-[0_0_51px_0] flex flex-row justify-between p-[2px_11.8px_1.8px_14px] w-[306px] box-sizing-border">
+        <div class="m-[0.5px_10.5px_0.7px_0] inline-block w-[239.5px] break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#6D6D6D]">
+          지출
+        </div>
+        <img class="rotate-[179.561deg] w-[26.2px] h-[22.2px]" src="../assets/vectors/polygon_1_x2.svg" />
+      </div>
+      <div class="rounded-[20px] border-[2px_solid_rgba(0,0,0,0.06)] bg-[#FFFFFF] flex flex-row justify-between p-[2px_12.8px_1.8px_14px] w-[306px] box-sizing-border">
+        <div class="m-[0.5px_10.5px_0.7px_0] inline-block w-[238.5px] break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#6D6D6D]">
+          식비
+        </div>
+        <img class="rotate-[179.561deg] w-[26.2px] h-[22.2px]" src="../assets/vectors/polygon_2_x2.svg" />
+      </div>
+    </div>
+    <div class="relative m-[5px_29.6px_213px_0] inline-block break-words font-['Inter'] font-bold text-[16px] text-[#000000]">
+      메모
+    </div>
+    <div class="rounded-[20px] border-[2px_solid_rgba(0,0,0,0.06)] bg-[#FFFFFF] relative m-[0_0_4px_0] p-[104.5px_14px_103.5px_14px] w-[306px] box-sizing-border">
+      <span class="break-words font-['Inter'] font-semibold text-[16px] leading-[1.3] text-[#6D6D6D]">
+        점심 값 10,000원 지출
+      </span>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>

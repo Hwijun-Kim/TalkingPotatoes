c1a5c7d3d351f34fc72cadb02b278d5eac0d48db
M6F\student <qhdbs0802@naver.com>
2024-06-11 16:56:41 +0900

    홈 로그인 인덱스 앱 변경

--- project-TP/src/router/index.js
+++ project-TP/src/router/index.js
@@ -1,13 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/pages/Home.vue';
-import CreateItem from '@/pages/CreateItem.vue';
+import { createRouter, createWebHistory } from "vue-router";
+
+import CreateItem from "@/pages/CreateItem.vue";
+import Login from "@/pages/Login.vue";
+import Home from "@/pages/Home.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      { path: '/createItem', component: CreateItem },
-      { path: '/home', component: Home },
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Login, meta: { hideHeader: true } },
+    { path: "/home", component: Home },
+    { path: "/createItem", component: CreateItem },
+  ],
+});
 
 export default router;

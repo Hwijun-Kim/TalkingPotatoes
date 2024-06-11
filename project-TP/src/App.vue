f8f6a1db55818a736a303495dbf1485c192e561c
Hwijun-Kim <rlagnlwnsvv@naver.com>
2024-06-11 09:42:39 +0900

    project 생성

---
+++ project-TP/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <Header />
+      <router-view />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ project-TP/src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <span class="navbar-brand ps-2">Talking Potatoes</span>
+    </nav>
+</template>

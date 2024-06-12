71d916c358d68633f24e0c69a4ce897eed253677
pangminwhere <qoendqh@naver.com>
2024-06-12 13:54:07 +0900

    modified TotalView.vue

--- project-TP/src/pages/TotalView.vue
+++ project-TP/src/pages/TotalView.vue
@@ -3,264 +3,122 @@
         <br /><br />
         <h3>수입 / 지출 전체 조회</h3>
         <div class="basic-group-1">
-      <div class="card-item basic">
-        <div>
-          <div class="flex-table">
-            <div class="left-section">
-              <div class="flex-cell">
-                <div class="flex-table-item">
-                  <div class="item-text">
-                    <label for="date">시작 날짜</label>
-                  </div>
-                  <div class="item-input">
-                    <input
-                      type="date"
-                      class="form-control"
-                      id="date"
-                      name="date"
-                      style="text-align: center"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="flex-cell">
-                <div class="flex-table-item">
-                  <div class="item-text">
-                    <label for="type">수입/지출</label>
-                  </div>
-                  <div class="item-input">
-                    <select
-                      id="type"
-                      class="form-select"
-                      v-model="formData.type"
-                      @change="updateCategories"
-                    >
-                      <option value="spend">지출</option>
-                      <option value="income">수입</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="right-section">
-                <div class="flex-cell">
-                    <div class="flex-table-item">
-                    <div class="item-text">
-                        <label for="date">종료 날짜</label>
+            <div class="card-item basic">
+                <div>
+                    <div class="flex-table">
+                        <div class="left-section">
+                            <div class="flex-cell">
+                                <div class="flex-table-item">
+                                    <div class="item-text">
+                                        <label for="date">시작 날짜</label>
+                                    </div>
+                                    <div class="item-input">
+                                        <input
+                                            type="date"
+                                            class="form-control"
+                                            id="date"
+                                            name="date"
+                                            style="text-align: center"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-cell">
+                                <div class="flex-table-item">
+                                    <div class="item-text">
+                                        <label for="type">수입/지출</label>
+                                    </div>
+                                    <div class="item-input">
+                                        <select
+                                            id="type"
+                                            class="form-select"
+                                            v-model="formData.type"
+                                            @change="updateCategories"
+                                        >
+                                            <option value="spend">지출</option>
+                                            <option value="income">수입</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="right-section">
+                            <div class="flex-cell">
+                                <div class="flex-table-item">
+                                    <div class="item-text">
+                                        <label for="date">종료 날짜</label>
+                                    </div>
+                                    <div class="item-input">
+                                        <input
+                                            type="date"
+                                            class="form-control"
+                                            id="date"
+                                            name="date"
+                                            style="text-align: center"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex-cell">
+                                <div class="flex-table-item">
+                                    <div class="item-text">
+                                        <label for="category">카테고리</label>
+                                    </div>
+                                    <div class="item-input">
+                                        <select
+                                            class="form-select"
+                                            id="category"
+                                            v-model="formData.category"
+                                        >
+                                            <option
+                                                v-for="option in categoryOptions"
+                                                :key="option"
+                                                :value="option"
+                                            >
+                                                {{ option }}
+                                            </option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="center-section">
+                            <button type="button" class="search_btn">조회</button>
+                        </div>
                     </div>
-                    <div class="item-input">
-                        <input
-                        type="date"
-                        class="form-control"
-                        id="date"
-                        name="date"
-                        style="text-align: center"
-                        />
-                    </div>
-                    </div>
                 </div>
-                <div class="flex-cell">
-                <div class="flex-table-item">
-                  <div class="item-text">
-                    <label for="category">카테고리</label>
-                  </div>
-                  <div class="item-input">
-                    <select
-                      class="form-select"
-                      id="category"
-                      v-model="formData.category"
-                    >
-                      <option
-                        v-for="option in categoryOptions"
-                        :key="option"
-                        :value="option"
-                      >
-                        {{ option }}
-                      </option>
-                    </select>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div class="center-section">
-                <button type="button" class="search_btn">조회</button>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
         <br /><br />
         <div class="row tab">
             <ul class="list-group">
                 <li class="list-group-item list_size">
                     <table>
-                        <tr>
-                            <th class="th_width">날짜</th>
-                            <th class="th_width">분류</th>
-                            <th class="th_const">금액</th>
-                            <th class="th_width"></th>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                1,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                지출
-                            </td>
-                            <td>
-                                11,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                30,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                30,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                30,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                30,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                30,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                24.06.10
-                            </td>
-                            <td>
-                                수입
-                            </td>
-                            <td>
-                                30,835원
-                            </td>
-                            <td>
-                                <button
-                                    @click="viewDetails(transaction)"
-                                    type="button"
-                                    class="btn btn-outline"
-                                >
-                                    상세보기
-                                </button>
-                            </td>
-                        </tr>
-                    </table>           
+                        <thead>
+                            <tr>
+                                <th class="th_width">날짜</th>
+                                <th class="th_width">분류</th>
+                                <th class="th_const">금액</th>
+                                <th class="th_width"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="transaction in transactions" :key="transaction.id">
+                                <td>{{ transaction.date }}</td>
+                                <td>{{ transaction.inout ? '수입' : '지출' }}</td>
+                                <td>{{ transaction.money }}원</td>
+                                <td>
+                                    <button
+                                        @click="viewDetails(transaction.id)"
+                                        type="button"
+                                        class="btn btn-outline"
+                                    >
+                                        상세보기
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </li>
             </ul>
         </div>
@@ -268,38 +126,57 @@
 </template>
 
 <script>
+import { useAccountListStore } from '@/stores/store';
+import { reactive, onMounted, computed } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
-  data() {
-    return {
-      formData: {
-        date: "",
-        amount: "",
-        type: "spend",
-        category: "",
-        memo: "",
-      },
-      incomeCategories: ["급여", "용돈", "기타"],
-      spendCategories: ["쇼핑", "식비", "교통비", "생활비", "문화생활", "기타"],
-      categoryOptions: [],
-    };
-  },
-  methods: {
-    updateCategories() {
-      if (this.formData.type === "income") {
-        this.categoryOptions = this.incomeCategories;
+  setup() {
+    const store = useAccountListStore();
+    const router = useRouter();
+    const { state, fetchLists } = store;
+
+    const formData = reactive({
+      startDate: "",
+      endDate: "",
+      type: "spend",
+      category: ""
+    });
+
+    const transactions = computed(() => state.lists);
+
+    const updateCategories = () => {
+      if (formData.type === "income") {
+        store.state.categoryOptions = store.state.incomeCategories;
       } else {
-        this.categoryOptions = this.spendCategories;
+        store.state.categoryOptions = store.state.spendCategories;
       }
-    },
-  },
-  mounted() {
-    this.updateCategories();
-  },
+    };
+
+    const fetchTransactions = () => {
+      fetchLists();
+    };
+
+    const viewDetails = (id) => {
+      router.push({ name: 'UpdateItem', params: { id } });
+    };
+
+    onMounted(() => {
+      fetchTransactions();
+      updateCategories();
+    });
+
+    return {
+      formData,
+      categoryOptions: computed(() => formData.type === "income" ? store.state.incomeCategories : store.state.spendCategories),
+      transactions,
+      updateCategories,
+      fetchTransactions,
+      viewDetails
+    };
+  }
 };
-
 </script>
-
 <style scoped>
     .list_size {
         height: 40px;

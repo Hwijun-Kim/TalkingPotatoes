0f836351ef34feee08f2c5c1b80a2a16fc4c54b3
minjeonj0 <whalswjd99@gmail.com>
2024-06-12 13:56:03 +0900

    자잘구리

--- project-TP/src/router/index.js
+++ project-TP/src/router/index.js
@@ -8,13 +8,13 @@ import UpdateItem from "@/pages/UpdateItem.vue";
 import TotalView from "@/pages/TotalView.vue";
 import Summary from "@/pages/Summary.vue";
 
-// const viewsIdGuard = (to, from) => {
-//   // totalView/:id 경로는 반드시 이전 경로가
-//   // /totalView, /totalView:id 인 경우만 내비게이션 허용함
-//   if (from.name !== "totalView" && from.name !== "totalView/id") {
-//       return false;
-//   }
-// }
+const viewsIdGuard = (to, from) => {
+  // totalView/:id 경로는 반드시 이전 경로가
+  // /totalView, /totalView:id 인 경우만 내비게이션 허용함
+  if (from.name !== "totalView" && from.name !== "totalView/id") {
+      return false;
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(),
@@ -34,18 +34,18 @@ const router = createRouter({
   ],
 });
 
-// router.beforeEach((to)=> {
-//   //to.query에 속성이 존재할 경우 제거된 경로로 재이동
-//   if (to.query && Object.keys(to.query).length > 0) {
-//       return { path:to.path, query:{}, params: to.params };
-//   }
-// })
+router.beforeEach((to)=> {
+  //to.query에 속성이 존재할 경우 제거된 경로로 재이동
+  if (to.query && Object.keys(to.query).length > 0) {
+      return { path:to.path, query:{}, params: to.params };
+  }
+})
 
-// router.afterEach((to, from, failure)=> {
-//   if (isNavigationFailure(failure)) {
-//       console.log("@@ 내비게이션 중단 : ", failure)
-//       return { name:"home" };
-//   }
-// })
+router.afterEach((to, from, failure)=> {
+  if (isNavigationFailure(failure)) {
+      console.log("@@ 내비게이션 중단 : ", failure)
+      return { name:"home" };
+  }
+})
 
 export default router;

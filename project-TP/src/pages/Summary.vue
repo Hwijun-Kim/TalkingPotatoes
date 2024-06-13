5f9aeda39fca8b632c0fd3632a760b0d17a2c249
pangminwhere <qoendqh@naver.com>
2024-06-13 13:22:15 +0900

    update Summary, plus SearchPotato

--- project-TP/src/pages/Summary.vue
+++ project-TP/src/pages/Summary.vue
@@ -46,12 +46,13 @@
             <p>총 지출 : {{ currentExpense }}원</p>
           </div>
           <div class="chart">
-            <div class="chartimage">
-              <img v-if="currentIncome-currentExpense > 0" src="@/assets/ExcellentPotato.jpg" alt="ExcellentPotato" width="200px" height="200px">
-              <img v-if="currentIncome-currentExpense <= 0" src="@/assets/SadPotato.jpg" alt="SadPotato" width="200px" height="200px">
-            </div>
-            <p>순수익 : {{ currentIncome - currentExpense }}원</p>
+          <div class="chartimage">
+            <img v-if="currentIncome - currentExpense > 0" src="@/assets/ExcellentPotato.jpg" alt="ExcellentPotato" width="200px" height="200px">
+            <img v-else-if="currentIncome - currentExpense <= 0 && currentExpense > 0" src="@/assets/SadPotato.jpg" alt="SadPotato" width="200px" height="200px">
+            <img v-else src="@/assets/SearchPotato.webp" alt="SearchPotato" width="200px" height="200px">
           </div>
+          <p>순수익 : {{ currentIncome - currentExpense }}원</p>
+        </div>
         </div>
       </div>
     </div>
